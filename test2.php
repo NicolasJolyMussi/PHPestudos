2693e1cf9a2027c7f02315f1bae2e27cf888d32e
Nícolas Mussi <nicobocojoly@gmail.com>
2025-08-26 17:39:40 -0300

    test2.php

---
+++ test2.php
@@ -0,0 +1,47 @@
+<?php
+
+// Variáveis  e tipos de dados
+echo "Variáveis e Tipos de Dados <br></br>";
+
+echo "test <br></br>";
+
+// Variáveis
+$nome = "Nícolas";
+
+echo "Olá meu nome é $nome <br></br>";
+
+$sobreNome = "Mussi";
+
+$nomeCompleto = $nome . "" . $sobreNome;
+
+echo "O meu nome completo é $nomeCompleto <br></br>";
+
+//Topos de Dados
+//inteiro
+$idade = 18;
+
+echo "Eu tenho $idade anos. <br></br>";
+
+$somaIdade = $idade + 5;
+
+echo "A soma da minha idade mais 5 é $somaIdade <br></br>";
+
+// float * numeros quebrados
+
+$altura = 1.80;
+
+echo "Minha altura é $altura <br></br>";
+
+$somarAltura = $altura + .03;
+
+echo "Somando 03 a mais na minha altura fica $somarAltura <br></br>";
+
+//boolean
+//verdadeiro ou false / Verdadeiro é igual a 1 e False é igual a 0
+
+$maiorDeIdade = true;
+
+$podeDirigir = false;
+
+echo "$maiorDeIdade <br></br>";
+
